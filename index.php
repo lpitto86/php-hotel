8ba3cad7d881c85f755249a7e7c7bf547610234c
lpitto86 <luca.p481@gmail.com>
2024-02-08 17:27:46 +0100

    Inizializzazione

---
+++ index.php
@@ -0,0 +1,67 @@
+<?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+    for($i = 0; $i < count($hotels); $i++;) {
+      echo '<hr>';
+      echo '<div>'.hotels[$i]['name'].'</div>';
+      echo '<div>'.hotels[$i]['description'].'</div>';
+      echo '<div>'.hotels[$i]['parking'].'</div>';
+      echo '<div>'.hotels[$i]['vote'].'</div>';
+      echo '<div>'.hotels[$i]['distance_to_center'].'</div>';
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Hotels</title>
+</head>
+<body>
+      <header>
+            <h1>
+                  Hotels
+            </h1>
+      </header>
+</body>
+</html>
